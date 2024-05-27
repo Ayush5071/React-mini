6882ed973a67732bd6a3a948841bd43b9f4275fc
Ayush5071 <ayusht5071@gmail.com>
2024-05-28 01:31:45 +0530

    completed the TODO app with local storage.

--- 10todocontext/src/App.jsx
+++ 10todocontext/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState,useEffect } from 'react'
 import './App.css'
 import { TodoProvider } from './contexts'
+import { Todoform, Todoitems } from './components'
 
 function App() {
   const [todos,setTodos] = useState([])
@@ -19,7 +20,7 @@ function App() {
     setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo,completed : !prevTodo.completed} : prevTodo))
   }
   useEffect(() => {
-    JSON.parse(localStorage.getItem('todos'));
+    const todos=JSON.parse(localStorage.getItem('todos'));
     if(todos && todos.length > 0){
       setTodos(todos);
     }
@@ -37,9 +38,14 @@ function App() {
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
+                      <Todoform/>
                     </div>
                     <div className="flex flex-wrap gap-y-3">
-
+                      {todos.map((todo)=>(
+                        <div key={todo.id} className='w-full'>
+                          <Todoitems todo={todo} />
+                        </div>
+                      ))}
                     </div>
                 </div>
             </div>
